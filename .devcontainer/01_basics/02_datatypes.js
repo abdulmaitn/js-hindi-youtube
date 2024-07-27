1e492fa85a8f20e62a7030a501361ed513dbdece
abdulmaitn <159881994+abdulmaitn@users.noreply.github.com>
2024-07-27 07:50:11 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict" // treat all JS code as newar version
+
+// alert ( 3 + 3); // We are using nodejs, not browser
+
+console.log(3 + 3) 
+console.log("Hitesh");
+
+
+let name = "Hitesh";
+let age = 18;
+let isLoggedIn=false;
+
+
+// number => 2 to pwer 53
+// bigInt
+//string => ""
+//boolean => true.false
+//null => standalone value
+//undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof "hitesh");
+console.log(typeof age);
+console.log(typeof null);  // object
+console.log(typeof undefined); // undefined
