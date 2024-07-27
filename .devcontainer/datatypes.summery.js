f0a8f8fc8245b480e252d1d47f9085de5034ff4f
abdulmaitn <159881994+abdulmaitn@users.noreply.github.com>
2024-07-27 10:05:58 +0000

    datatype summery

---
+++ .devcontainer/datatypes.summery.js
@@ -0,0 +1,41 @@
+// TYpes of Datatypes
+
+// Primitive
+
+// 7 types : String, Number, Boolean, Null, undefined, Symbol,BigInt,
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail;
+
+const id = Symbol(`123`)
+const anotherId = Symbol(`123`);
+
+
+const bigNumber = 234234342342334n
+
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heroes = ["shaktiman", "naagara", "doga"]
+
+let myObj{
+    name: "Matin",
+    age : 22,
+
+
+}
+
+const myFunction = function(){
+  console.log("Hello World!");
+}
+
+
+console.log(typeof bigNumber);
