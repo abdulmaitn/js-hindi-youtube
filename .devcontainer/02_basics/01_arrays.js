b52055feba7924ef0ad2d4241c0d4da950b2901e
abdulmaitn <159881994+abdulmaitn@users.noreply.github.com>
2024-07-28 09:02:43 +0000

    arrays

---
+++ .devcontainer/02_basics/01_arrays.js
@@ -0,0 +1,47 @@
+// array
+
+// const myArr = [0, 1, 2, 3, 4,5]
+// console.log([0]);
+
+// const myheors = ["shaktiman", "naagraj"]
+// const myArr2 = new Array(1,2,3,4);
+// console.log(myArr[0]);
+
+
+
+// Array methods
+let myArr = [0, 1, 2, 3, 4, 5];
+// myArr.push(6); // Adds 6 to the end of the array
+// myArr.push(7); // Adds 7 to the end of the array
+// myArr.pop(); // Removes the last element from the array
+// console.log(myArr); // Outputs the array after push and pop operations
+
+// myArr.unshift(9); // Adds 9 to the beginning of the array
+// console.log(myArr); // Outputs the array after unshift operation
+
+// myArr.shift(); // Removes the first element from the array
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// let newArr = myArr.join()
+
+// console.log()
+// console.log(myArr); // Outputs the array after shift operation
+// console.log(newArr)
+// console.log(typeof newArr)
+
+
+// slice, splice
+
+console.log("A ", myArr);
+
+let myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ",myArr);
+
+let myn2 = myArr.splice(1,3)
+console.log("C ", myArr);
+console.log(myn2);
+
